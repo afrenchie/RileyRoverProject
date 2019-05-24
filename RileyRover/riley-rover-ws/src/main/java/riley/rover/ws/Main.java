017c71d82c914be2b0b4d0465dadad1e89d381f3
Quentin MALINGE <quentin.malinge@gmail.com>
2019-05-24 15:10:18 +0200

    Update main

--- RileyRover/riley-rover-ws/src/main/java/riley/rover/ws/Main.java
+++ RileyRover/riley-rover-ws/src/main/java/riley/rover/ws/Main.java
@@ -14,21 +14,6 @@ public class Main {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-
-		/*
-		 * System.out.println("Test lecture bytes -> abcdef"); byte[] decoded = new
-		 * byte[6]; byte[] encoded = new byte[] { (byte) 198, (byte) 131, (byte) 130,
-		 * (byte) 182, (byte) 194, (byte) 135 }; byte[] key = new byte[] { (byte) 167,
-		 * (byte) 225, (byte) 225, (byte) 210 }; for (int i = 0; i < encoded.length;
-		 * i++) { decoded[i] = (byte) (encoded[i] ^ key[i & 0x3]); }
-		 * System.out.println("Decoded bytes = " + decoded);
-		 * 
-		 * System.out.println("Decoded = " + new String(decoded,
-		 * StandardCharsets.UTF_8));
-		 */
-		/*
-		 * 
-		 */
 	}
 
 }
